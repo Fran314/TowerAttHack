961b98b6ee96770748903eb90b26125039ddb354
Fran314 <41571076+Fran314@users.noreply.github.com>
2020-02-29 10:56:00 +0100

    Added Basic Movement Mecanic

---
+++ TowerAtthack/Assets/GoToPoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToPoint : MonoBehaviour
+{
+    public Transform point;
+
+    public float speed = 2f;
+    public float min_dist = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(point.transform.position, transform.position) <= min_dist)
+            if (point.childCount > 0)
+                point = point.GetChild(0);
+        transform.Translate((point.transform.position - transform.position).normalized * speed * Time.deltaTime);
+    }
+}
